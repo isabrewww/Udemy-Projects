6f235c73b83bd6c74d820afd0d76a909d86f24e7
isabrewww <112526908+isabrewww@users.noreply.github.com>
2022-09-17 08:19:15 -0500

    Update Pizza_Order.py
    added ".lower()" to the variables to simplify the if/else statements.

--- Pizza_Order.py
+++ Pizza_Order.py
@@ -1,24 +1,24 @@
 print("Welcome to Brewster Pizza Deliveries!")
 
-pizzaSize = input("What size pizza would you like? S, M, or L: ")
-pepperoni = input("Would you like to add pepperoni? Y or N: ")
-extraCheese = input("Would you like extra cheese? Y or N: ")
+pizzaSize = input("What size pizza would you like? S, M, or L: ").lower()
+pepperoni = input("Would you like to add pepperoni? Y or N: ").lower()
+extraCheese = input("Would you like extra cheese? Y or N: ").lower()
 bill = 0
 
-if pizzaSize == "s" or pizzaSize == "S":
+if pizzaSize == "s":
     bill += 15
-elif pizzaSize == "m" or pizzaSize == "M":
+elif pizzaSize == "m":
     bill += 20
-elif pizzaSize == "l" or pizzaSize == "L":
+elif pizzaSize == "l":
     bill += 25
 
-if pepperoni == "y" or pepperoni == "Y":
-    if pizzaSize == "s" or pizzaSize == "S":
+if pepperoni == "y":
+    if pizzaSize == "s":
         bill += 2
     else:
         bill += 3
 
-if extraCheese == "y" or extraCheese == "Y":
+if extraCheese == "y":
     bill += 1
 
 print(f"Your final bill is: ${bill}.")
